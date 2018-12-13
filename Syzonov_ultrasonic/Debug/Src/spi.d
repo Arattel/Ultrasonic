a35ddf0ea675d03e5af93fe7120523bb5b5818ad
Arattel <sizonov2000@gmail.com>
2018-12-13 21:20:16 +0200

    first commit

---
+++ Syzonov_ultrasonic/Debug/Src/spi.d
@@ -0,0 +1,108 @@
+Src/spi.o: ../Src/spi.c /home/arattel/STM/testsem/Inc/spi.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /home/arattel/STM/testsem/Inc/stm32f4xx_hal_conf.h \
+ /home/arattel/STM/testsem/Inc/main.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cm4.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/arattel/STM/testsem/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h \
+ /home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h \
+ /home/arattel/STM/testsem/Inc/gpio.h
+
+/home/arattel/STM/testsem/Inc/spi.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/home/arattel/STM/testsem/Inc/stm32f4xx_hal_conf.h:
+
+/home/arattel/STM/testsem/Inc/main.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f411xe.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/arattel/STM/testsem/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2c_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_i2s_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_spi.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_tim_ex.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_hcd.h:
+
+/home/arattel/STM/testsem/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_ll_usb.h:
+
+/home/arattel/STM/testsem/Inc/gpio.h:
